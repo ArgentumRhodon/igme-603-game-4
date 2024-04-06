d4e539e65ba5aa4aad4b13a58aac0104343a9f9f
ArgentumRhodon <lac2559@g.rit.edu>
2024-04-05 22:58:27 -0400

    update rod upgrade ui on purchase

--- FishingGame/Assets/Scripts/Shop/UpgradeShop.cs
+++ FishingGame/Assets/Scripts/Shop/UpgradeShop.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class UpgradeShop : Shop
 {
+    public GameObject PurchasedPrefab;
+
     public override void PopulateStorePage()
     {
         foreach(ValueEntry item in items)
         {
             Rod rod = (Rod)item.item;
             GameObject storeObject = Instantiate(UIPrefab, this.transform);
-            storeObject.GetComponent<RodUpgrade>().Populate(rod, item.value);
+            storeObject.GetComponent<RodUpgrade>().Populate(rod, item.value, storeObject, PurchasedPrefab);
         }
     }
 }
